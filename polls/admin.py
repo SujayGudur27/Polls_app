87c6f21d573e4741b8b8dc93b481b7999e2f81b6
Sujay Gudur <sujaysgudur69@gmail.com>
2026-02-11 14:07:22 +0530

    Part 7: Enhanced Django admin with TabularInline, fieldsets, list display, filters, search and custom admin styling

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
-     
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+
+    # Group fields into sections
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date Information", {
+            "fields": ["pub_date"],
+            "classes": ["collapse"],
+        }),
+    ]
+
+    # Show choices inline inside question
+    inlines = [ChoiceInline]
+
+    # Columns in change list page
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+    # Filter sidebar
+    list_filter = ["pub_date"]
+
+    # Search bar
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
